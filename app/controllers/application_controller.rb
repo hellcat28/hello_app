03bc2af610cec4a2a25e15e78035aea4387fd07c
Luat <dltrongluat@gmail.com>
2018-10-11 08:16:31 +0000

    new update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello mtfk2!"
+    render html: "Hi, my name is Luat!"
   end
 end
